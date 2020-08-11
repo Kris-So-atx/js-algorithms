7fe2caef04b85514f640bb50135aad572832dfad
kristinasotskova <krisnooklyn@gmail.com>
2020-08-10 23:08:34 -0400

    New

---
+++ Hello, Name or World!.js
@@ -0,0 +1,12 @@
+Hello, Name or World!
+
+    function hello(name) {
+//   const convNames = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+        if (name == "johN") {
+            return "Hello, John!";
+        } else if (name == "alice") {
+            return "Hello, Alice!";
+        } else {
+            return "Hello, World!"
+        }
+    }
