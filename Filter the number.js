cf1cc102ac738edc8f52a211b075242ab6a07beb
kristinasotskova <krisnooklyn@gmail.com>
2020-08-20 22:52:13 -0400

    solution new

---
+++ Filter the number.js
@@ -0,0 +1,9 @@
+Filter the number
+
+function FilterString(value) {
+    let result = '';
+    for(let i = 0; i < value.length; i++){
+        if(!isNaN(value[i])) result+= value[i];
+    }
+    return +result;
+}
